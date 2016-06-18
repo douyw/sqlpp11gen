4f44e1d5cfb2e90e85567e182a62c5c6c68d4d0b
Dou Yongwang <douyongwang@gmail.com>
2016-06-18 01:16:52 +0000

    generator fixed

--- lib/generators/sqlpp11gen/sqlpp11gen_generator.rb
+++ lib/generators/sqlpp11gen/sqlpp11gen_generator.rb
@@ -1,4 +1,5 @@
 require 'rails/generators'
+require 'active_record'
 require 'generators/sqlpp11gen/cxxes_context'
 require 'erb'
 
@@ -9,43 +10,94 @@ module Sqlpp11gen
     class Sqlpp11genGenerator < Rails::Generators::NamedBase
 #      source_root File.expand_path('../templates', __FILE__)
 
-      desc "Some description of my generator here"
- 
       # Commandline options can be defined here using Thor-like options:
-#      class_option :my_opt, :type => :boolean, :default => false, :desc => "My Option"
-
-      # I can later access that option using:
-      # options[:my_opt]
-
+      argument :output_path,         :type => :string, :required => false, :desc => "setup output path for the generated file. The default output path is 'app/cxxes'."
+      argument :filename_prefix,     :type => :string, :required => false, :desc => "setup a filename prefix for the generated file. The default one is 'tab_'."
+      argument :filename_ext,        :type => :string, :required => false, :desc => "setup a filename ext for the generated file. The default one is 'hpp'."
 
       def self.source_root
         @source_root ||= File.join(File.dirname(__FILE__), 'templates')
       end
 
+      def initialize(args, *options)
+        super(args, *options)
+        initialize_views_variables
+      end
 
-      def create_helper_file
+      def create_cxx_header_file
 
-#        tablename = plural_name
-        tableklass = table_name.to_s.constantize
-        cols = tableklazz.columns
+        tableklass = table_name.to_s.singularize.camelize.constantize
 
         ctx = ::CxxesContext.new
         ctx.tablename = table_name
         ctx.tableklass = tableklass
-        ctx.cols = cols
-
-#        puts "File.dirname(__FILE__) #{File.dirname(__FILE__)}"
+        ctx.cols = tableklass.columns
 
         template = File.read(File.join(File.dirname(__FILE__), "templates", "index.h.erb"))
 
-#    create_file "app/cxxes/#{file_name}.hpp", <<-FILE
-#module #{class_name}Helper
-#  attr_reader :#{plural_name}, :#{plural_name.singularize}
-#end
-#    FILE
-        create_file "app/cxxes/#{file_name}.hpp", ERB.new(template).result(ctx.template_binding)
+        path = output_path || 'app/cxxes'
+        prefix = filename_prefix || 'tab_'
+        ext = filename_ext || 'hpp'
+
+        create_file "#{path}/#{prefix}#{file_name}.#{ext}", ERB.new(template).result(ctx.template_binding)
 
       end
+
+      protected
+
+      def initialize_views_variables
+      end
+
+      def columns
+        retrieve_columns.reject {|c| excluded?(c.name) }.map do |c|
+          new_attribute(c.name, c.type.to_s)
+        end
+      end
+
+      def excluded_columns_names
+        %w[_id _type id created_at updated_at]
+      end
+
+      def excluded_columns_pattern
+        [
+          /.*_checksum/,
+          /.*_count/,
+        ]
+      end
+
+      def excluded_columns
+        options['excluded_columns']||[]
+      end
+
+      def excluded?(name)
+        excluded_columns_names.include?(name) ||
+        excluded_columns_pattern.any? {|p| name =~ p } ||
+        excluded_columns.include?(name)
+      end
+
+      def retrieve_columns
+        if defined?(ActiveRecord) == "constant" && ActiveRecord.class == Module 
+          rescue_block ActiveRecord::StatementInvalid do
+            @model_name.constantize.columns
+          end
+        else
+          rescue_block do
+            @model_name.constantize.fields.map {|c| c[1] }
+          end
+        end
+      end
+
+      def new_attribute(name, type)
+        ::Rails::Generators::GeneratedAttribute.new(name, type)
+      end
+
+      def rescue_block(exception=Exception)
+        yield if block_given?
+      rescue exception => e
+        say e.message, :red
+        exit
+      end
+
     end
   end
 end
